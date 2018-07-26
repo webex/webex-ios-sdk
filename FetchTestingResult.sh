81ea39cd305675baf3c5286670f91b0cd68930cc
panzh <panzh@cisco.com>
2018-07-26 14:30:59 +0800

    update test script

--- FetchTestingResult.sh
+++ FetchTestingResult.sh
@@ -1,3 +1,16 @@
+if [ "$Email" == "" ]; then
+	echo "lost email address!"
+	exit 0
+fi
+
+if $IsIntegrationTestingTask -eq "true" ; then
+	echo "start Fetch testing result..."
+else
+	echo "not the integration test task return normal..."
+	exit 0
+fi
+
+
 for ((i=0; i<=25; i++)); do
 buildEmailTitle=`curl -u $Email:$EmailPWD --silent "https://mail.google.com/mail/feed/atom/important" | awk -F '<title>' '{for (i=3; i<=NF; i++) {print $i"\n"}}' | awk -F '</title>' '{for (i=1; i<=NF; i=i+2) {print $i"\n"}}' | awk -F: '/^BuildNumber:'"$TRAVIS_JOB_ID"'/'`
 
