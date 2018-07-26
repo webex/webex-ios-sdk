686649a82888507a1b76e3addd88ab24696610a0
panzh <panzh@cisco.com>
2018-07-26 13:59:00 +0800

    update test script

--- FetchTestingResult.sh
+++ FetchTestingResult.sh
@@ -1,18 +1,3 @@
-if [ "$Email" == "" ]; then
-	echo "lost email address!"
-	exit 0
-fi
-
-if $IsIntegrationTestingTask -eq "true" ; then
-	echo "start Integration testing..."
-	sendemail -f "$Email" -t "$Email" -u "WebexSDK build by Travis" -m "BranchName=$TRAVIS_BRANCH\nBuildNumber=$TRAVIS_JOB_ID\nPullRequestNumber=$TRAVIS_PULL_REQUEST" -s smtp.gmail.com -o tls=yes -xu "$Email" -xp "$EmailPWD"
-else
-	echo "not the integration test task return normal..."
-	exit 0
-fi
-
-sleep 60
-
 for ((i=0; i<=25; i++)); do
 buildEmailTitle=`curl -u $Email:$EmailPWD --silent "https://mail.google.com/mail/feed/atom/important" | awk -F '<title>' '{for (i=3; i<=NF; i++) {print $i"\n"}}' | awk -F '</title>' '{for (i=1; i<=NF; i=i+2) {print $i"\n"}}' | awk -F: '/^BuildNumber:'"$TRAVIS_JOB_ID"'/'`
 
