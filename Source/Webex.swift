c2ebda4e6cd6530ea99186c8c6d9eeb1f60634c7
kliu <kyle.lzy@gmail.com>
2020-06-05 02:42:56 +0800

    Version number

--- Source/Webex.swift
+++ Source/Webex.swift
@@ -62,7 +62,7 @@ public class Webex {
     /// The version number of this Cisco Webex iOS SDK.
     ///
     /// - since: 1.2.0
-    public static let version = "2.6.0"
+    public static let version = "2.5.0.8"
     
     /// The logger for this SDK.
     ///
