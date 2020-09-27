1e0d73f6116e87cfd72b1b58d2e2e67fcf68fc94
yonshi <yonshi@cisco.com>
2020-09-27 14:41:05 +0800

    update API document

---
+++ build/WebexSDK.build/Release-iphoneos/WebexSDK.build/DerivedSources/WebexSDK_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char WebexSDKVersionString[];
+ extern const double WebexSDKVersionNumber;
+
+ const unsigned char WebexSDKVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:WebexSDK  PROJECT:WebexSDK-2600" "\n";
+ const double WebexSDKVersionNumber __attribute__ ((used)) = (double)2600.;
