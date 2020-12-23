a2e7ea1630b8daf34a9b74927ef13f94eedfe54e
MrShiY <708984366@qq.com>
2020-12-23 10:48:16 +0800

    change version number

--- Source/Webex.swift
+++ Source/Webex.swift
@@ -62,7 +62,7 @@ public class Webex {
     /// The version number of this Cisco Webex iOS SDK.
     ///
     /// - since: 1.2.0
-    public static let version = "2.6.0"
+    public static let version = "2.6.0.1"
     
     /// The logger for this SDK.
     ///
